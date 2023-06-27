c582364c04052a3b67d64860526a296449ee8951
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-06-27 18:36:07 +0300

    commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -28,11 +28,11 @@ function createImegCardMarcup(items) {
       <li class="gallery__item">
         <a class="gallery__link" 
          href="${original}">
-        <img class="gallery__image" 
-         src="${preview}" 
-         alt="${description}" />
-        </a>
-      </li>`;
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
+ </li>`;
     })
     .join("");
 }
